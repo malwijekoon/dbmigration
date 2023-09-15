f5e462d0f1d9c6607c2baa2a7054a6e00907db97
LK220084.MWijekoon <malshani.wijekoon@empowerteams.io>
2023-09-15 10:01:51 +0530

    added scripts

---
+++ src/main/resources/db/migration/v1.0.0_release/Table/V1.0.0_1692236819__Create_Table_Talent.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS talent.profile (
+    id uuid DEFAULT uuid_generate_v4(),
+    nickname varchar(255),
+    thumbnail varchar(255),
+    is_active boolean,
+    firstname varchar(255),
+    lastname varchar(255),
+    middlename varchar(255),
+    age INTEGER,
+    CONSTRAINT profile_id_pk PRIMARY KEY (id)
+    );
